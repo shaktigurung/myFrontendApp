87b7f7f5307da170553ba97b3db3274f365dda6c
shaktigurung <mr.shakti.gurung@gmail.com>
2019-02-27 12:00:26 +1100

    Javascript added for change of tab on add or remove todos

--- main.js
+++ main.js
@@ -7,14 +7,29 @@ function getTodoText() {
     return document.querySelector('.textfield').value;
 }
 
+function updateRemaining() {
+    const remaining = document.querySelector('.remaining');
+    const todos = document.querySelectorAll('.todo');
+    remaining.innerText = todos;
+}
 function addTodo(){
    const todo = document.querySelector('.todo');
    const newTodo = todo.cloneNode(true);
    newTodo.querySelector('.title').innerText = getTodoText();
    todo.parentElement.insertBefore(newTodo, todo);
    clearInput('.textfield');
+   updateRemaining();
 }
 
+function clearCompleted() {
+    const todos = document.querySelectorAll('.todo');
+    for(let todo of todos){
+        if (todo.querySelector('input').checked ==true){
+            todo.remove();
+        }
+    }
+    updateRemaining();
+}
 function filter(filterName, button) {
     document.querySelector('.selected').classList.remove('selected');
     button.classList.add('selected');
